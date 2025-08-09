c778483e8e3001d17f87a036bc0f6754a4e9bd0b
Your Name <your.email@example.com>
2025-08-08 21:52:31 -0400

    git

---
+++ lua/plugins/fugitive.lua
@@ -0,0 +1,30 @@
+return {
+	"tpope/vim-fugitive",
+	cmd = {
+		"G",
+		"Git",
+		"Gdiffsplit",
+		"Gread",
+		"Gwrite",
+		"Ggrep",
+		"GMove",
+		"GDelete",
+		"GBrowse",
+		"GRemove",
+		"GRename",
+		"Glgrep",
+		"Gedit",
+	},
+	ft = {"fugitive"},
+	config = function ()
+		local keymap = vim.keymap.set
+
+		keymap("n", "<leader>gs", "<cmd>Git<CR>", { desc = "Git status" })
+		keymap("n", "<leader>gd", "<cmd>Gdiffsplit<CR>", { desc = "Git diff" })
+		keymap("n", "<leader>gc", "<cmd>Git commit<CR>", { desc = "Git commit" })
+		keymap("n", "<leader>gl", "<cmd>Git log<CR>", { desc = "Git log" })
+		keymap("n", "<leader>gp", "<cmd>Git push<CR>", { desc = "Git push" })
+		keymap("n", "<leader>gP", "<cmd>Git pull<CR>", { desc = "Git pull" })
+		keymap("n", "<leader>gb", "<cmd>Git blame<CR>", { desc = "Git blame" })
+	end,
+}
